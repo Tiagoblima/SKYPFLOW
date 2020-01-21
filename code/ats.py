099993d1bbf7ce0aab9b18c9196dcd510276b23f
Tiago de Lima <tiago.blima02@gmail.com>
2020-01-21 17:05:49 -0300

    Ajustes realizados no código

--- code/ats.py
+++ code/ats.py
@@ -105,7 +105,7 @@ sentences = []
 
 originals = []
 
-fp1 = open("../Resources/glove.6B.300d.txt", "r", encoding='utf-8')
+fp1 = open("Resources/glove.6B.300d.txt", "r", encoding='utf-8')
 glove_emb = {}
 for line in fp1:
     temp = line.split(" ")
@@ -116,7 +116,7 @@ print("Embedding done")
 # %%
 essay_type = '4'
 
-fp = open("../Resources/training_set_rel32.tsv", 'r', encoding="ascii", errors="ignore")
+fp = open("Resources/training_set_rel32.tsv", 'r', encoding="ascii", errors="ignore")
 fp.readline()
 for line in fp:
     temp = line.split("\t")
@@ -129,7 +129,7 @@ print("range min - ", min(originals), " ; range max - ", max(originals))
 range_min = min(originals)
 range_max = max(originals)
 
-fp = open("../Resources/training_set_rel3.tsv", 'r', encoding="ascii", errors="ignore")
+fp = open("Resources/training_set_rel32.tsv", 'r', encoding="ascii", errors="ignore")
 fp.readline()
 sentences = []
 for line in fp:
@@ -285,7 +285,7 @@ y_pred = sf_1.predict([x_val])
 y_val_fin = [int(round(a * (range_max - range_min) + range_min)) for a in y_val]
 
 # %%
-y_pred_fin = [int(round(a * (range_max - range_min) + range_min)) for a in y_pred.reshape(354).tolist()]
+y_pred_fin = [int(round(a * (range_max - range_min) + range_min)) for a in y_pred.reshape(58).tolist()]
 
 # %%
 print(cohen_kappa_score(y_val_fin, y_pred_fin, weights="quadratic"))
--- code/model.py
+++ code/model.py
@@ -1,32 +1,23 @@
-import keras.layers  as  klayers
-from keras.preprocessing.text import text_to_word_sequence
-from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Dense, LSTM, Input, Embedding, GlobalAveragePooling1D, Concatenate, Activation, Lambda, \
-    BatchNormalization, Convolution1D, Dropout
-from keras.preprocessing.text import Tokenizer
 import numpy as np
-import nltk
-from quadratic_weighted_kappa import QWK
-from sklearn.metrics import cohen_kappa_score
-from keras.models import Model
-from tensorflow.keras import backend as K
-from keras.engine.topology import Layer, InputSpec
-from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping
-from keras import regularizers
-from keras import initializers
-from scipy import stats
 import tensorflow as tf
+from keras import initializers
+from keras.callbacks import EarlyStopping
+from keras.engine.topology import Layer, InputSpec
+from keras.layers import Dense, LSTM, Input, Embedding, Concatenate, Lambda
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.preprocessing.text import Tokenizer
+from scipy import stats
+from tensorflow.keras import backend as K
 
 EMBEDDING_DIM = 300
 MAX_NB_WORDS = 4000
 MAX_SEQUENCE_LENGTH = 500
 DELTA = 20
 
-fp1 = open("C:\\Users\\Tiago\\PycharmProjects\\Automatic-Text-Scoring\\webapp\\models\\glove.6B.300d.txt", "r",
-           encoding='utf-8')
+embed_file = open("Resources/glove.6B.300d.txt", "r",encoding='utf-8')
 glove_emb = {}
-for line in fp1:
+for line in embed_file:
     temp = line.split(" ")
     glove_emb[temp[0]] = np.asarray([float(i) for i in temp[1:]])
 
@@ -128,7 +119,7 @@ def SKIPFLOW(embedding_matrix, vocab_size, lstm_dim=50, lr=1e-4, lr_decay=1e-6, 
     dense = Dense(128, activation=activation, kernel_initializer=initializers.glorot_normal(seed=seed))(dense)
     dense = Dense(64, activation=activation, kernel_initializer=initializers.glorot_normal(seed=seed))(dense)
     out = Dense(1, activation="sigmoid")(dense)
-    model = Model(inputs=[e, trad_feats], outputs=[out])
+    model = Model(inputs=[e], outputs=[out])
     adam = Adam(lr=lr, decay=lr_decay)
     model.compile(loss="mean_squared_error", optimizer=adam, metrics=["MSE"])
     return model
@@ -143,14 +134,14 @@ def init(essay_type='4'):
     texts = []
     originals = []
 
-    fp = open("C:\\Users\\Tiago\\PycharmProjects\\Automatic-Text-Scoring\\webapp\\models\\training_set_rel3.tsv", 'r',
+    fp = open("Resources/glove.6B.300d.txt", 'r',
               encoding="ascii", errors="ignore")
     fp.readline()
     sentences = []
     doctovec = []
     for line in fp:
         temp = line.split("\t")
-        if (temp[1] == essay_type):  ## why only 4 ?? - evals in prompt specific fashion
+        if temp[1] == essay_type:  ## why only 4 ?? - evals in prompt specific fashion
             texts.append(temp[2])
             originals.append(float(temp[6]))
             line = temp[2].strip()
@@ -169,7 +160,7 @@ def init(essay_type='4'):
     embedding_matrix = np.zeros((vocab_size, EMBEDDING_DIM))
 
     for word, i in word_index.items():
-        if (i >= len(word_index)):
+        if i >= len(word_index):
             continue
         if word in glove_emb:
             embedding_matrix[i] = glove_emb[word]
@@ -179,7 +170,7 @@ def init(essay_type='4'):
                     activation="relu", seed=None)
 
     sf_1.load_weights(
-        "C:\\Users\\Tiago\\PycharmProjects\\Automatic-Text-Scoring\\webapp\\models\\weights/" + str(
+        "Resources/weights/" + str(
             essay_type) + "_weights.h5")
     # print("Loaded Model from disk")
 
